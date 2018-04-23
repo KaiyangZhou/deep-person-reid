81a45d0a0fa8dd52128028f373e684f2182c8014
KaiyangZhou <k.zhou@qmul.ac.uk>
2018-04-23 16:08:24 +0100

    add comments

--- train_img_model_xent_htri.py
+++ train_img_model_xent_htri.py
@@ -34,6 +34,7 @@ parser.add_argument('--height', type=int, default=256,
 parser.add_argument('--width', type=int, default=128,
                     help="width of an image (default: 128)")
 parser.add_argument('--split-id', type=int, default=0, help="split index")
+# CUHK03-specific setting
 parser.add_argument('--cuhk03-labeled', action='store_true',
                     help="whether to use labeled images, if false, detected images are used (default: False)")
 parser.add_argument('--cuhk03-classic-split', action='store_true',
