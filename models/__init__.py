399db23a374789d49ef670bef3a358842be77217
KaiyangZhou <k.zhou@qmul.ac.uk>
2018-04-24 17:07:43 +0100

    add hacnn

--- models/__init__.py
+++ models/__init__.py
@@ -3,12 +3,14 @@ from __future__ import absolute_import
 from .ResNet import *
 from .DenseNet import *
 from .MuDeep import *
+from .HACNN import *
 
 __factory = {
     'resnet50': ResNet50,
     'densenet121': DenseNet121,
     'resnet50m': ResNet50M,
     'mudeep': MuDeep,
+    'hacnn': HACNN,
 }
 
 def get_names():
