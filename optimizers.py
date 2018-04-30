1351c0aa5d111102b77d7df45247367a81f6f440
KaiyangZhou <k.zhou@qmul.ac.uk>
2018-04-30 17:50:17 +0100

    incorporate rmsprop

--- optimizers.py
+++ optimizers.py
@@ -7,5 +7,7 @@ def init_optim(optim, params, lr, weight_decay):
         return torch.optim.Adam(params, lr=lr, weight_decay=weight_decay)
     elif optim == 'sgd':
         return torch.optim.SGD(params, lr=lr, momentum=0.9, weight_decay=weight_decay)
+    elif optim == 'rmsprop':
+        return torch.optim.RMSprop(params, lr=lr, momentum=0.9, weight_decay=weight_decay)
     else:
         raise KeyError("Unsupported optim: {}".format(optim))
