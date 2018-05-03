fbc6425ff1b615a58a980d9a768720ec2d96809f
KaiyangZhou <k.zhou@qmul.ac.uk>
2018-05-03 15:04:01 +0100

    fix bug

--- models/SEResNet.py
+++ models/SEResNet.py
@@ -13,8 +13,7 @@ import torchvision
 Code imported from https://github.com/Cadene/pretrained-models.pytorch
 """
 
-__all__ = ['SENet', 'senet154', 'se_resnet50', 'se_resnet101', 'se_resnet152',
-           'se_resnext50_32x4d', 'se_resnext101_32x4d']
+__all__ = ['SEResNet50']
 
 pretrained_settings = {
     'senet154': {
