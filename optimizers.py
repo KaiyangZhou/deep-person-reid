4d4d70eb94e2ea96b0712120ef9ebb5938788b57
KaiyangZhou <k.zhou@qmul.ac.uk>
2018-07-02 13:56:46 +0100

    add amsgrad

--- optimizers.py
+++ optimizers.py
@@ -4,6 +4,8 @@ import torch
 def init_optim(optim, params, lr, weight_decay):
     if optim == 'adam':
         return torch.optim.Adam(params, lr=lr, weight_decay=weight_decay)
+    elif optim == 'amsgrad':
+        return torch.optim.Adam(params, lr=lr, weight_decay=weight_decay, amsgrad=True)
     elif optim == 'sgd':
         return torch.optim.SGD(params, lr=lr, momentum=0.9, weight_decay=weight_decay)
     elif optim == 'rmsprop':
