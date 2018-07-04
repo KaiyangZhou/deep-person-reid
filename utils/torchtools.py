0299bca11f8954cd8320d38ac691f4db484f1204
KaiyangZhou <k.zhou@qmul.ac.uk>
2018-07-04 15:53:50 +0100

    add set_bn_to_eval

--- utils/torchtools.py
+++ utils/torchtools.py
@@ -8,3 +8,11 @@ def adjust_learning_rate(optimizer, base_lr, epoch, stepsize, gamma=0.1):
     lr = base_lr * (gamma ** (epoch // stepsize))
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
+
+
+def set_bn_to_eval(m):
+    classname = m.__class__.__name__
+    if classname.find('BatchNorm') != -1:
+        m.eval()
+        m.weight.requires_grad = False
+        m.bias.requires_grad = False
