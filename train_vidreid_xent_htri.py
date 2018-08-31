7c62ab2ed8e72e722271b70199fbb698becb5ecb
KaiyangZhou <k.zhou@qmul.ac.uk>
2018-08-31 11:04:44 +0100

    fig bug: replace dataset.train in triplet sampler with new_train

--- train_vidreid_xent_htri.py
+++ train_vidreid_xent_htri.py
@@ -148,7 +148,7 @@ def main():
 
     trainloader = DataLoader(
         ImageDataset(new_train, transform=transform_train),
-        sampler=RandomIdentitySampler(dataset.train, args.train_batch, args.num_instances),
+        sampler=RandomIdentitySampler(new_train, args.train_batch, args.num_instances),
         batch_size=args.train_batch, num_workers=args.workers,
         pin_memory=pin_memory, drop_last=True,
     )
