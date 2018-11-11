8ca8be28c204dbc37cff76e77691f29045773aa2
KaiyangZhou <k.zhou@qmul.ac.uk>
2018-11-11 23:02:20 +0000

    update test_cython.py: add cuhk03's comparison

--- torchreid/eval_cylib/test_cython.py
+++ torchreid/eval_cylib/test_cython.py
@@ -9,7 +9,17 @@ sys.path.insert(0, osp.dirname(osp.abspath(__file__)) + '/../..')
 
 from torchreid.eval_metrics import evaluate
 
-print("=> Compare running time")
+"""
+Test the speed of cython-based evaluation code. The speed improvements
+can be much bigger when using the real reid data, which contains a larger
+amount of query and gallery images.
+
+Note: you might encounter the following error:
+  'AssertionError: Error: all query identities do not appear in gallery'.
+This is normal because the inputs are random numbers. Just try again.
+"""
+
+print("*** Compare running time ***")
 
 setup = '''
 import sys
@@ -18,7 +28,7 @@ import numpy as np
 sys.path.insert(0, osp.dirname(osp.abspath(__file__)) + '/../..')
 from torchreid.eval_metrics import evaluate
 num_q = 10
-num_g = 200
+num_g = 300
 max_rank = 5
 distmat = np.random.rand(num_q, num_g) * 20
 q_pids = np.random.randint(0, num_q, size=num_q)
@@ -26,12 +36,22 @@ g_pids = np.random.randint(0, num_g, size=num_g)
 q_camids = np.random.randint(0, 5, size=num_q)
 g_camids = np.random.randint(0, 5, size=num_g)
 '''
+
+print("=> Using market1501's metric")
 pytime = timeit.timeit('evaluate(distmat, q_pids, g_pids, q_camids, g_camids, max_rank, use_cython=False)', setup=setup, number=20)
 cytime = timeit.timeit('evaluate(distmat, q_pids, g_pids, q_camids, g_camids, max_rank, use_cython=True)', setup=setup, number=20)
-print("Python time: {}".format(pytime))
-print("Cython time: {}".format(cytime))
+print("Python time: {} s".format(pytime))
+print("Cython time: {} s".format(cytime))
 print("Cython is {} times faster than python\n".format(pytime / cytime))
 
+print("=> Using cuhk03's metric")
+pytime = timeit.timeit('evaluate(distmat, q_pids, g_pids, q_camids, g_camids, max_rank, use_metric_cuhk03=True, use_cython=False)', setup=setup, number=20)
+cytime = timeit.timeit('evaluate(distmat, q_pids, g_pids, q_camids, g_camids, max_rank, use_metric_cuhk03=True, use_cython=True)', setup=setup, number=20)
+print("Python time: {} s".format(pytime))
+print("Cython time: {} s".format(cytime))
+print("Cython is {} times faster than python\n".format(pytime / cytime))
+
+"""
 print("=> Check precision")
 
 num_q = 10
@@ -47,3 +67,4 @@ cmc, mAP = evaluate(distmat, q_pids, g_pids, q_camids, g_camids, max_rank, use_c
 print("Python:\nmAP = {} \ncmc = {}\n".format(mAP, cmc))
 cmc, mAP = evaluate(distmat, q_pids, g_pids, q_camids, g_camids, max_rank, use_cython=True)
 print("Cython:\nmAP = {} \ncmc = {}\n".format(mAP, cmc))
+"""
