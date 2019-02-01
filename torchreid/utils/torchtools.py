760d68ebd1781b67b3abcca2968dd36968d14975
KaiyangZhou <k.zhou@qmul.ac.uk>
2019-02-01 22:43:47 +0000

    bug fix

--- torchreid/utils/torchtools.py
+++ torchreid/utils/torchtools.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 
+from collections import OrderedDict
+
 import torch
 import torch.nn as nn
 
