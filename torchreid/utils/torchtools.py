800df43feeb365ef332e5b302b0155ffd43e6931
KaiyangZhou <k.zhou@qmul.ac.uk>
2019-02-01 00:50:28 +0000

    check if output is list

--- torchreid/utils/torchtools.py
+++ torchreid/utils/torchtools.py
@@ -84,6 +84,9 @@ def accuracy(output, target, topk=(1,)):
         maxk = max(topk)
         batch_size = target.size(0)
 
+        if isinstance(output, (tuple, list)):
+            output = output[0]
+
         _, pred = output.topk(maxk, 1, True, True)
         pred = pred.t()
         correct = pred.eq(target.view(1, -1).expand_as(pred))
