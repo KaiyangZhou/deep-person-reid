dee01229dabf3c3fc2113f67bde6987e113397d5
KaiyangZhou <k.zhou@qmul.ac.uk>
2019-04-23 11:34:52 +0100

    remove division by 1e+06 when counting model size

--- torchreid/utils/torchtools.py
+++ torchreid/utils/torchtools.py
@@ -201,18 +201,22 @@ def open_specified_layers(model, open_layers):
 def count_num_param(model):
     """Counts number of parameters in a model.
 
+    Args:
+        model (nn.Module): network model.
+
     Examples::
         >>> from torchreid.utils import count_num_param
         >>> model_size = count_num_param(model)
     """
-    num_param = sum(p.numel() for p in model.parameters()) / 1e+06
+    num_param = sum(p.numel() for p in model.parameters())
 
     if isinstance(model, nn.DataParallel):
         model = model.module
 
     if hasattr(model, 'classifier') and isinstance(model.classifier, nn.Module):
         # we ignore the classifier because it is unused at test time
-        num_param -= sum(p.numel() for p in model.classifier.parameters()) / 1e+06
+        num_param -= sum(p.numel() for p in model.classifier.parameters())
+    
     return num_param
 
 
@@ -224,7 +228,7 @@ def load_pretrained_weights(model, weight_path):
         - Can automatically deal with keys containing "module.".
 
     Args:
-        model (nn.Module): model.
+        model (nn.Module): network model.
         weight_path (str): path to pretrained weights.
 
     Examples::
