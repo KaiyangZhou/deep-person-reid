6829805723eddec8226512e783f7447bc383bfdc
KaiyangZhou <k.zhou@qmul.ac.uk>
2019-07-03 13:40:23 +0100

    [version] 0.7.8 => 0.8.0

--- torchreid/__init__.py
+++ torchreid/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-__version__ = '0.7.8'
+__version__ = '0.8.0'
 __author__ = 'Kaiyang Zhou'
 __description__ = 'Deep learning person re-identification in PyTorch'
 
