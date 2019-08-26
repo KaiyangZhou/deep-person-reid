5507b38c862295848493f3a96fdf32bfc1870997
KaiyangZhou <k.zhou@qmul.ac.uk>
2019-08-26 13:26:10 +0100

    update v0.9.4 to v1.0.0

--- torchreid/__init__.py
+++ torchreid/__init__.py
@@ -1,9 +1,10 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-__version__ = '0.9.4'
+__version__ = '1.0.0'
 __author__ = 'Kaiyang Zhou'
 __description__ = 'Deep learning person re-identification in PyTorch'
+__url__ = 'https://github.com/KaiyangZhou/deep-person-reid'
 
 from torchreid import (
     engine,
