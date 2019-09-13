7a73dca4ac8e42137c480b0f798c1be01ae6e66b
kaiyangzhou <k.zhou@surrey.ac.uk>
2019-09-13 23:23:13 +0100

    bug fix: img_path->path

--- torchreid/utils/tools.py
+++ torchreid/utils/tools.py
@@ -112,7 +112,7 @@ def read_image(path):
             img = Image.open(path).convert('RGB')
             got_img = True
         except IOError:
-            print('IOError incurred when reading "{}". Will redo. Don\'t worry. Just chill.'.format(img_path))
+            print('IOError incurred when reading "{}". Will redo. Don\'t worry. Just chill.'.format(path))
             pass
     return img
 
