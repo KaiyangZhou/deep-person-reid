45091f4f85fe40c150b8dd3ac270eb507fcdb02b
KaiyangZhou <k.zhou@surrey.ac.uk>
2019-11-04 10:47:49 +0000

    add __homepage__

--- torchreid/__init__.py
+++ torchreid/__init__.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 
 __version__ = '1.0.6'
 __author__ = 'Kaiyang Zhou'
+__homepage__ = 'https://kaiyangzhou.github.io/'
 __description__ = 'Deep learning person re-identification in PyTorch'
 __url__ = 'https://github.com/KaiyangZhou/deep-person-reid'
 
