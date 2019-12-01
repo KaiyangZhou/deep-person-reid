ac7bd6748440a165784194b98783b76bc993e735
KaiyangZhou <k.zhou@surrey.ac.uk>
2019-12-01 02:39:29 +0000

    import data, engine, etc. in torchreid/__init__.py

--- torchreid/__init__.py
+++ torchreid/__init__.py
@@ -1,5 +1,7 @@
 from __future__ import print_function, absolute_import
 
+from . import data, engine, losses, metrics, models, optim, utils
+
 __version__ = '1.0.8'
 __author__ = 'Kaiyang Zhou'
 __homepage__ = 'https://kaiyangzhou.github.io/'
