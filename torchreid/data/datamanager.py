94d6b02e406b320165324a157234538bbdb0ef02
ZXYFrank <49837632+ZXYFrank@users.noreply.github.com>
2022-02-23 21:23:47 +0800

    Update datamanager.py
    add **kwargs to all occurrences of `init_image_dataset ` in `datamanager.py`

--- torchreid/data/datamanager.py
+++ torchreid/data/datamanager.py
@@ -248,7 +248,8 @@ class ImageDataManager(DataManager):
                     split_id=split_id,
                     cuhk03_labeled=cuhk03_labeled,
                     cuhk03_classic_split=cuhk03_classic_split,
-                    market1501_500k=market1501_500k
+                    market1501_500k=market1501_500k,
+                    **kwargs
                 )
                 trainset_t.append(trainset_t_)
             trainset_t = sum(trainset_t)
@@ -297,7 +298,8 @@ class ImageDataManager(DataManager):
                 split_id=split_id,
                 cuhk03_labeled=cuhk03_labeled,
                 cuhk03_classic_split=cuhk03_classic_split,
-                market1501_500k=market1501_500k
+                market1501_500k=market1501_500k,
+                **kwargs
             )
             self.test_loader[name]['query'] = torch.utils.data.DataLoader(
                 queryset,
@@ -319,7 +321,8 @@ class ImageDataManager(DataManager):
                 split_id=split_id,
                 cuhk03_labeled=cuhk03_labeled,
                 cuhk03_classic_split=cuhk03_classic_split,
-                market1501_500k=market1501_500k
+                market1501_500k=market1501_500k,
+                **kwargs
             )
             self.test_loader[name]['gallery'] = torch.utils.data.DataLoader(
                 galleryset,
