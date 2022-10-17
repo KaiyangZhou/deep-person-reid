28e866f50d78106433db4799396e21e137044383
Cameron <cameron@pop-os.localdomain>
2022-10-17 11:32:10 -0700

    init

--- optimized_vector.py
+++ optimized_vector.py
@@ -32,16 +32,30 @@ search_query = json.load(f)
 global_end_time = datetime.now().isoformat()
 global_start_time = '2022-10-13T07:17:15.892850'
 
+def download_images(elastic_docs):
+    join_time = []
+    for num, doc in enumerate(tqdm(elastic_docs)):
+        join_start = time.time()
+        url_of_image = str(doc['fields']['s3_presigned'][0])
+        #print(url_of_image)
+        instances_id = doc['_id']
+        index = doc['_index']
+        full_file_name = os.path.join(input_path, f"{instances_id}={index}.jpg")
+        urllib.request.urlretrieve(url_of_image, full_file_name)
+        join_time.append(time.time() - join_start)
+    return join_time
+
 
 def main():
     global_end_time = datetime.now().isoformat()
     #search_query['query']['bool']['filter'][1]['range']['inferenced_timestamp']['gte'] = global_end_time
-    json_info = es.search(index = "snl-ghostrunner-*", body = search_query)
+    json_info = es.search(index = "snl-ghostrunner-*", body = search_query, size = 500)
     elastic_docs = json_info["hits"]["hits"]
     # iterate the docs returned by API call
     if os.path.isdir(f'{input_path}') == False:
         os.makedirs(f'{input_path}')
     print("Images Are Downloading")
+    print(elastic_docs)
     counter = 0
     with open('data.json', 'w', encoding='utf-8') as f:
         json.dump(elastic_docs, f, ensure_ascii=False, indent=4)
@@ -50,23 +64,21 @@ def main():
         model_path='./osnet_ms_d_c.pth.tar',
         device='cuda'
         )
-    print(elastic_docs)
-    join_time = []
+    join_time = download_images(elastic_docs)
+    #print(elastic_docs)
     upload_time = []
     ML_time  = []
-    for num, doc in enumerate(tqdm(elastic_docs)):
-        join_start = time.time()
-        url_of_image = str(doc['fields']['s3_presigned'][0])
-        instances_id = doc['_id']
-        index = doc['_index']
-        full_file_name = os.path.join(input_path, f"{instances_id}.jpg")
-        urllib.request.urlretrieve(url_of_image, full_file_name)
-        join_time.append(time.time() - join_start)
+    print('Running anaylsis')
+    for num, image in enumerate(tqdm(os.listdir(input_path))):
         ml_start = time.time()
-        image = full_file_name
-        features = extractor(image) 
+        image2 = f'{input_path}{image}'
+        features = extractor(image2) 
         features = features.cpu().detach().numpy()
         features = features[0]
+        split = image.split('=')
+        instances_id = split[0]
+        index = split[1][:-4]
+        #print(instances_id,  index)
         document = {'person_vectors': features}
         ML_time.append(time.time() - ml_start)
         counter += 1
